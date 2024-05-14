ade4548e6b323e49c39f874ec0bbea76b7a48301
Ahan Saripalli <96845815+A-Sarpall@users.noreply.github.com>
2024-05-14 14:38:31 -0500

    Update insta.py

--- insta.py
+++ insta.py
@@ -16,14 +16,14 @@ def main():
     following_file = st.sidebar.file_uploader("Choose a JSON file", key="following", type=["json"])
 
     if followers_file is not None and following_file is not None:
-        followers_1_data = [json.load(followers_file)]
-        following_data = [json.load(following_file)]
+        followers_1_data = json.load(followers_file)
+        following_data = json.load(following_file)
 
         # Extract accounts from followers_1.json
-        followers_1_accounts = [entry['string_list_data'][0]['value'] for entry in followers_1_data]
+        followers_1_accounts = [entry['name'] for entry in followers_1_data['users']]
 
         # Extract accounts from following.json
-        following_accounts = [entry['string_list_data'][0]['value'] for entry in following_data['relationships_following']]
+        following_accounts = [entry['username'] for entry in following_data['users']]
 
         # Create a list of accounts in following but not in followers_1
         accounts_not_followers = ["[instagram.com/" + account + "](https://www.instagram.com/" + account + ")" for account in following_accounts if account not in followers_1_accounts]
