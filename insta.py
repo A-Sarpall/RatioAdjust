2ec05b368b91a7f3451b7f2702ec61434620fadd
Ahan Saripalli <96845815+A-Sarpall@users.noreply.github.com>
2024-05-14 14:11:13 -0500

    Update insta.py

--- insta.py
+++ insta.py
@@ -21,10 +21,10 @@ def main():
         following_data = json.load(following_file)
 
         # Extract accounts from followers_1.json
-        followers_1_accounts = [entry['string_list_data'][0]['value'] for entry in followers_1_data]
+        followers_1_accounts = [entry['name'] for entry in followers_1_data['profile_and_chaining'][0]['profile_and_chaining'][0]['chaining']['user']]
 
         # Extract accounts from following.json
-        following_accounts = [entry['string_list_data'][0]['value'] for entry in following_data['relationships_following']]
+        following_accounts = [entry['username'] for entry in following_data['following']]
 
         # Create a list of accounts in following but not in followers_1
         accounts_not_followers = ["[instagram.com/" + account + "](https://www.instagram.com/" + account + ")" for account in following_accounts if account not in followers_1_accounts]
