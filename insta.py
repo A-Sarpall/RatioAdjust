5ba20d727ca2a524a2e0d9fde2691d58f7596e21
Frootyloops44 <frootyloops44@gmail.com>
2024-05-16 13:26:06 -0500

    properly deleted function

--- insta.py
+++ insta.py
@@ -1,39 +1,36 @@
 import streamlit as st
 import json
-def main():
-    st.title("Ratio Adjuster")
-    st.text("See which Instagram accounts you follow that don't follow you back")
-    st.write("---")
-    st.text("Upload the followers_1.json and following.json files")
-    st.text("by downloading your Instagram data")
-    
-    # File upload for followers_1.json
-    st.sidebar.title('Upload followers_1.json')
-    followers_file = st.sidebar.file_uploader("Choose a JSON file", key="followers", type=["json"])
+st.title("Ratio Adjuster")
+st.text("See which Instagram accounts you follow that don't follow you back")
+st.write("---")
+st.text("Upload the followers_1.json and following.json files")
+st.text("by downloading your Instagram data")
 
-    # File upload for following.json
-    st.sidebar.title('Upload following.json')
-    following_file = st.sidebar.file_uploader("Choose a JSON file", key="following", type=["json"])
+# File upload for followers_1.json
+st.sidebar.title('Upload followers_1.json')
+followers_file = st.sidebar.file_uploader("Choose a JSON file", key="followers", type=["json"])
 
-    if followers_file is not None and following_file is not None:
-        followers_1_data = json.load(followers_file)
-        following_data = json.load(following_file)
+# File upload for following.json
+st.sidebar.title('Upload following.json')
+following_file = st.sidebar.file_uploader("Choose a JSON file", key="following", type=["json"])
 
-        # Extract accounts from followers_1.json
-        followers = [entry['string_list_data'][0]['value'] for entry in followers_1_data]
+if followers_file is not None and following_file is not None:
+    followers_1_data = json.load(followers_file)
+    following_data = json.load(following_file)
 
-        # Extract accounts from following.json
-        following = [entry['string_list_data'][0]['value'] for entry in following_data['relationships_following']]
+    # Extract accounts from followers_1.json
+    followers = [entry['string_list_data'][0]['value'] for entry in followers_1_data]
 
-        # Create a list of accounts in following but not in followers_1
-        not_following = [account for account in following if account not in followers]
+    # Extract accounts from following.json
+    following = [entry['string_list_data'][0]['value'] for entry in following_data['relationships_following']]
 
-        st.divider()
-        st.write("Accounts that don't follow you back:")
-        for account in not_following:
-            st.markdown(f"[instagram.com/{account}](https://www.instagram.com/{account})")
+    # Create a list of accounts in following but not in followers_1
+    not_following = [account for account in following if account not in followers]
+
+    st.divider()
+    st.write("Accounts that don't follow you back:")
+    for account in not_following:
+        st.markdown(f"[instagram.com/{account}](https://www.instagram.com/{account})")
     else:
+        st.divider()
         st.write("Files are Empty :(")
-        
-if __name__ == "__main__":
-    main()
